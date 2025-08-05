cabfc4c570fa98335d9912c8d72d60c10043b5fd
flightbee <dmitri.torchilo@gmail.com>
2025-08-06 01:44:23 +0300

    .

--- infra/terraform/provisioners.tf
+++ infra/terraform/provisioners.tf
@@ -5,7 +5,7 @@ resource "null_resource" "update_inventory" {
       echo '[all]' > ../ansible/inventory
       echo '${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} ansible_user=${var.ssh_user} ansible_ssh_private_key_file=~/.ssh/id_ed25519' >> ../ansible/inventory
       echo "---" > ../ansible/group_vars/all.yml
-      echo "bot_dir: /home/${var.ssh_user}/bot" >> ../ansible/group_vars/all.yml
+      echo "bot_dir: /opt/bot" >> ../ansible/group_vars/all.yml
       echo "ansible_python_interpreter: /usr/bin/python3" >> ../ansible/group_vars/all.yml
     EOT
     interpreter = ["/bin/bash", "-c"]
@@ -19,7 +19,7 @@ resource "null_resource" "run_ansible" {
       sleep 30
       cd ../ansible && ansible-playbook playbook.yml
     EOT
-    interpreter = ["/bin/bash", "-c"]
+    interpreter = ["/usr/bin/python3", "-c"]
   }
   depends_on = [null_resource.update_inventory]
 }
