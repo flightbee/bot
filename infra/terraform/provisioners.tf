3255f341a159eba44915172df1cc5972ff53f431
flightbee <dmitri.torchilo@gmail.com>
2025-08-06 00:14:10 +0300

    .

--- infra/terraform/provisioners.tf
+++ infra/terraform/provisioners.tf
@@ -1,12 +1,12 @@
 resource "null_resource" "update_inventory" {
   provisioner "local-exec" {
     command = <<EOT
-      echo '[all]
-      ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} ansible_user=${var.ssh_user} ansible_ssh_private_key_file=~/.ssh/id_ed25519' > ../ansible/inventory.ini
-      
-      # Создаём переменные для Ansible
-      echo "bot_dir: /home/${var.ssh_user}/bot" > ../ansible/group_vars/all.yml
-      echo "terraform_vm_ip: ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip}" >> ../ansible/group_vars/all.yml
+      mkdir -p ../ansible/group_vars
+      echo '[all]' > ../ansible/inventory.ini
+      echo '${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} ansible_user=${var.ssh_user} ansible_ssh_private_key_file=${var.public_key}' >> ../ansible/inventory.ini
+      echo "---" > ../ansible/group_vars/all.yml
+      echo "bot_dir: /home/${var.ssh_user}/bot" >> ../ansible/group_vars/all.yml
+      echo "ansible_python_interpreter: /usr/bin/python3" >> ../ansible/group_vars/all.yml
     EOT
     interpreter = ["/bin/bash", "-c"]
   }
@@ -15,7 +15,11 @@ resource "null_resource" "update_inventory" {
 
 resource "null_resource" "run_ansible" {
   provisioner "local-exec" {
-    command = "sleep 30 && cd ../ansible && ansible-playbook -i inventory.ini deploy_bot.yml"
+    command = <<EOT
+      sleep 30
+      cd ../ansible && ansible-playbook -i inventory.ini deploy_bot.yml
+    EOT
+    interpreter = ["/bin/bash", "-c"]
   }
   depends_on = [null_resource.update_inventory]
 }
